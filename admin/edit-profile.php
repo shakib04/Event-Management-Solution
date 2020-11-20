4fcddaf3eda4476352571a360a57bc8cf377e99e
shakib <mdshakibulalam@gmail.com>
2020-11-20 21:54:11 +0600

    edit profile validation added

--- admin/edit-profile.php
+++ admin/edit-profile.php
@@ -1,31 +1,12 @@
 <?php
 
 require_once "common-codes/session-code.php";
-
-require_once "validation/save-profile.php";
-
-
+require_once "validation/edit-profile-validation.php";
 
 
 echo "<pre>";
 //print_r($admin);
 echo "</pre>";
-
-
-foreach ($admin as $user) {
-    if ($user->username == $_SESSION['username']) {
-        $fullname = $user->fname;
-        $email = $user->email;
-        $gender = $user->gender;
-        $contact = $user->contact;
-        $address = $user->address;
-        break;
-    }
-}
-
-
-
-
 
 ?>
 
@@ -58,8 +39,6 @@ foreach ($admin as $user) {
             margin: 30px auto;
             width: 800px;
         }
-
-        
     </style>
 </head>
 
@@ -70,16 +49,16 @@ foreach ($admin as $user) {
         <table>
             <tr>
                 <td>Full Name</td>
-                <td><input type="text" name="fullname" id="" value="<?php echo $fullname; ?>"></td>
+                <td><input type="text" name="fullname" id="" value="<?php echo $fullname; ?>"> <?php echo $err_fullname ?></td>
             </tr>
             <tr>
                 <td>Email</td>
                 <td>
-                    <input type="email" name="email" value="<?php echo $email; ?>" id="">
+                    <input type="email" name="email" value="<?php echo $email; ?>" id=""> <?php echo $err_email ?>
                 </td>
             </tr>
             <tr>
-                <td>Gender</td>
+                <td>Gender <?php echo $err_gender ?></td>
                 <td>
                     <input type="radio" name="gender" id="" value="Male" checked> Male
                     <input type="radio" name="gender" id="" value="Female"> Female
@@ -89,14 +68,14 @@ foreach ($admin as $user) {
             <tr>
                 <td>Phone Number</td>
                 <td>
-                    <input type="text" name="contact" id="" value="<?php echo $contact; ?>">
+                    <input type="text" name="contact" id="" value="<?php echo $contact; ?>"> <?php echo $err_contact; ?>
                 </td>
             </tr>
 
             <tr>
                 <td>Local Address</td>
                 <td>
-                    <textarea name="address" id="" value="<?php echo $address; ?>"> Dhaka</textarea>
+                    <textarea name="address" id=""> <?php echo $address; ?></textarea> <?php echo $err_address ?>
                 </td>
             </tr>
 
