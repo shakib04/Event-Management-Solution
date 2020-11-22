a57c99dd3698ed6a55c7c30e4d337768a5423f64
Bithi <farjanaislambithi89@gmail.com>
2020-11-22 11:11:29 +0600

    user

---
+++ user/logout.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+
+<a href="login.php">Log out </a>
+
+
+</body>
+</html>
---
+++ user/profile.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <title>Profile</title>
+		   
+    </head>
+    <body>
+	     <?php include "header.php";?>
+		 <div>
+        <h2>My Profile Details</h2>
+        <table>
+            <tr>
+                <td>username:</td>
+                <td>Bithi</td>
+            </tr>
+            <tr>
+                <td>Full Name:</td>
+                <td>Farjana Islam</td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td>bithi89@gmail.com</td>
+            </tr>
+
+            <tr>
+                <td>Gender:</td>
+                <td>Female</td>
+            </tr>
+
+
+            <tr>
+                <td>Phone Number:</td>
+                <td>01318640801</td>
+            </tr>
+
+            <tr>
+                <td>Location</td>
+                <td>Dhaka</td>
+            </tr>
+
+            <tr>
+
+                <td colspan="2"><a href="edit-profile.php">Edit Profile</a></td>
+            </tr>
+        </table>
+    </div>
+	</body>
+</html>
