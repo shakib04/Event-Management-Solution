37aff315817467253e12cf806863fc74ea00331a
shakib <mdshakibulalam@gmail.com>
2020-12-14 23:11:56 +0600

    session added

--- homepage.php
+++ homepage.php
@@ -1,21 +1,5 @@
 <?php
 
-session_start();
-
-if (isset($_SESSION['username']) and isset($_SESSION['type'])) {
-
-    $type = strtolower($_SESSION['type']);
-    if (strtolower($_SESSION['type']) == "admin") {
-        header("location:admin/views/dashboard-admin.php");
-    } elseif ($type == "client" or $type == "user") {
-        header("location:user/");
-    } elseif ($type == "vendor") {
-        header("location:vendor/");
-    } elseif ($type == "planner") {
-        header("location:eventplanner/");
-    }
-}
-
 // echo "<h3> Hi,  " . $_SESSION['username'] . "</h3>";
 
 
--- php-codes/session-code.php
+++ php-codes/session-code.php
@@ -1,13 +1,16 @@
 <?php
 session_start();
 
-if ( !isset($_SESSION['username']) ) {
-    header("location: homepage.php");
-}
+if (isset($_SESSION['username']) and isset($_SESSION['type'])) {
 
-if (isset($_POST['logout'])) {
-    session_unset();
-    session_destroy();
-    header("location: homepage.php");
+    $type = strtolower($_SESSION['type']);
+    if (strtolower($_SESSION['type']) == "admin") {
+        header("location:admin/views/dashboard-admin.php");
+    } elseif ($type == "client" or $type == "user") {
+        header("location:user/");
+    } elseif ($type == "vendor") {
+        header("location:vendor/");
+    } elseif ($type == "planner") {
+        header("location:eventplanner/");
+    }
 }
-?>
