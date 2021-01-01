f71bdfd641d80fd0c1c23cba761486be5014311c
shakib <mdshakibulalam@gmail.com>
2021-01-01 21:51:14 +0600

    user log out fixed

--- user/views/mypagehead.php
+++ user/views/mypagehead.php
@@ -1,34 +1,37 @@
   <?php
     require_once "session-code.php";
-?>
-<html> 
-<head>
-    
-    <title>USER</title>
-    <link rel="stylesheet" type="text/css" href="css/mystyles.css">
-    
-</head>
+    ?>
+  <html>
 
-<body>
+  <head>
 
-     <form action="" method="post">
-        <input type="submit" value="Log Out,<?php echo $_SESSION['username']; ?>"class="log-out-button" name="logout">
-    </form>
-	
-    <div class="layout grid-cont">
-        
-        <div class="navigation grid-item">
-		    <a href="user.php">Homepage</a>
-            <a href="book_event.php">Book an Event</a>
-            <a href="Event.php">Event set design </a>
-			<a href="myorder.php">Order </a>
-            <a href="our_clients.php">Our clients </a>
-            <a href="services_we_provide.php">services we provide </a>
-            <a href="myprofile.php">My Profile</a>
-			
-			
-        </div>
-        <div class="search-bar ">
-            <input type="text" placeholder="What are you looking for?"> <button>Search</button>
-        </div>
-	</html>	
+      <title>USER</title>
+      <link rel="stylesheet" type="text/css" href="css/mystyles.css">
+
+  </head>
+
+  <body>
+
+      <form action="" method="post">
+          <input type="submit" value="Log Out,<?php echo $_SESSION['username']; ?>" class="log-out-button" name="logout">
+      </form>
+      <div style="margin-bottom: 10px;"></div>
+
+      <div class="grid-cont">
+
+          <div class="navigation grid-item">
+              <a href="user.php">Homepage</a>
+              <a href="book_event.php">Book an Event</a>
+              <a href="Event.php">Event set design </a>
+              <a href="myorder.php">Order </a>
+              <a href="our_clients.php">Our clients </a>
+              <a href="services_we_provide.php">services we provide </a>
+              <a href="myprofile.php">My Profile</a>
+
+
+          </div>
+          <div class="search-bar ">
+              <input type="text" placeholder="What are you looking for?"> <button>Search</button>
+          </div>
+
+  </html>
