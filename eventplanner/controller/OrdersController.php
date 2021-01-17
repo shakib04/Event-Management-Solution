240d5089e5c1578026ec1fcae36f8103b435b496
shakib <mdshakibulalam@gmail.com>
2021-01-17 23:14:51 +0600

    task need to done

--- eventplanner/controller/OrdersController.php
+++ eventplanner/controller/OrdersController.php
@@ -1,1 +1,9 @@
 <?php
+require_once "../model/database-conn.php";
+
+function getRequestedUnpaOrder()
+{
+    $sql = "select * from `purchased_services_details` psd, `planner_services_list` psl where psd.service_id = psl.service_id and planner_username = '" . $_SESSION['username'] . "' and `planner_approve` = 'no';";
+    $data = getColumsValue($sql);
+    return $data;
+}
--- eventplanner/view/Orders.php
+++ eventplanner/view/Orders.php
@@ -7,6 +7,45 @@ require_once "../controller/OrdersController.php";
 include_once("nav-bar.php");
 ?>
 
-<div id="order-list">
+<style>
+    .order-list{
+        border: 1px solid black;
+        margin: 5px;
+        padding: 10px;
+        line-height: 1.5;
+    }
 
+    .btn{
+        padding: 5px;
+        color: white;
+        margin: 5px;
+    }
+    .green-btn{
+        background-color: green;
+    }
+
+    .red-btn{
+        background-color: red;
+    }
+</style>
+
+<div id="unapprove-order-list">
+    <?php
+    $unapprovedOrders = getRequestedUnpaOrder();
+    echo "<h1>Requested Order</h1>";
+    if (count($unapprovedOrders) ==0) {
+     echo "<h1>No Requested Order Found</h1>";
+    }else{
+        foreach ($unapprovedOrders as $order) {
+            echo "<div class='order order-list'>";
+            echo "Service Name: " . $order['service_name'] . "<br>";
+            echo "Requested By: " . $order['client_username'] . "<br>";
+            echo "Price: " . $order['price'] . "<br>";
+            echo "Payment Status: " . $order['status(paid/unpaid)'] . "<br>";
+            echo "Your Approve: " . $order['planner_approve'] . "<br>";
+            echo '<a class="btn green-btn" href="">Approve</a> <a class="btn red-btn" href="">Delete</a>';
+            echo "</div>";
+        }
+    }
+    ?>
 </div>
